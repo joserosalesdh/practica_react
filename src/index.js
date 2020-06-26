f2f885f93bde753118a893676d8be36cb62d545e
joserosalesdh <jm.rosales92@gmail.com>
2020-06-26 14:35:46 -0300

    practica de clase

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'; //Porque estamos por escribir un componente (un componente es una funcion)
+import ReactDOM from 'react-dom'; //Es la parte de la libreria que se encarga de renderizacion 
+import './index.css'; //es el css que se le va a aplicar a esta pagina
+import App from './App'; //
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
